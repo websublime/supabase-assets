06964a368a3e90ce6c3f4ff4e933125b2ff48e19
Miguel Ramos <miguel.marques.ramos@gmail.com>
2022-05-17 00:53:11 +0100

    feat: change mime type detection (#16)

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,6 +1,7 @@
 import {walk} from '../src/utils'
 import {expect, test} from '@jest/globals'
 import {resolve, join, basename} from 'path'
+import * as mime from 'mime-types'
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('Should walk directory', async () => {
@@ -18,4 +19,12 @@ test('Should retrive filenames', async () => {
   const filenames = files.map(file => basename(file))
 
   expect(filenames.length > 0).toBeTruthy()
+})
+
+test('Should retrive mime type', async () => {
+  const root = resolve(join(__dirname, '../src/jest.config.js'))
+
+  const type = mime.lookup(root)
+
+  expect(type).toEqual('application/javascript')
 })
